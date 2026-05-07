bfc2614dcd31cbc2b04c968c9e48c954ab2deb36
Yuliia Proshina <yuliakodalova@mail.ru>
2026-05-07 13:08:52 +0300

    add import ingredients

---
+++ backend/recipes/management/commands/import_ingredients.py
@@ -0,0 +1,27 @@
+import csv
+
+from django.core.management.base import BaseCommand
+
+from recipes.models import Ingredient
+
+
+class Command(BaseCommand):
+    """Импортирует ингредиенты из CSV-файла."""
+
+    def handle(self, *args, **kwargs):
+        with open('/app/data/ingredients.csv', encoding='utf-8') as file:
+            reader = csv.reader(file)
+            next(reader)
+            Ingredient.objects.bulk_create(
+                (
+                    Ingredient(
+                        name=row[0],
+                        measurement_unit=row[1]
+                    )
+                    for row in reader
+                ),
+                ignore_conflicts=True,
+            )
+        self.stdout.write(
+            self.style.SUCCESS('Ингредиенты импортированы.')
+        )
