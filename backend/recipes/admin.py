cb64a0283ef70e2edace0a91e5413964d5781b38
Yuliia Proshina <yuliakodalova@mail.ru>
2026-05-08 06:26:06 +0300

    change admin

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import Group
+from rest_framework.authtoken.models import TokenProxy
 
 from .models import (FavoriteRecipe, Ingredient, Recipe, RecipeIngredient,
                      ShoppingCart, Subscription, Tag, User)
@@ -61,3 +62,9 @@ class SubscriptionAdmin(admin.ModelAdmin):
 
 
 admin.site.unregister(Group)
+
+
+try:
+    admin.site.unregister(TokenProxy)
+except admin.sites.NotRegistered:
+    pass
