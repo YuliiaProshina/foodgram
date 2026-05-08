97e58c2ca8e4db1746a52c6649730c7da3952d41
mihan-pro <mihan-pro@mail.ru>
2026-05-08 20:02:04 +0400

    remove tokenProxy

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from django.contrib.auth.models import Group
-from rest_framework.authtoken.models import  Token, TokenProxy
+from rest_framework.authtoken.models import  Token
 
 from .models import (FavoriteRecipe, Ingredient, Recipe, RecipeIngredient,
                      ShoppingCart, Subscription, Tag, User)
@@ -62,7 +62,6 @@ class SubscriptionAdmin(admin.ModelAdmin):
 
 
 admin.site.unregister(Group)
-admin.site.unregister(TokenProxy)
 admin.site.unregister(Token)
 
 
