688a4df2fcedfe48370d97cec55844694e6c6382
mihan-pro <mihan-pro@mail.ru>
2026-05-09 01:37:13 +0400

    set JWT

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -20,7 +20,7 @@ INSTALLED_APPS = [
     'api',
     'recipes',
     'rest_framework',
-    'rest_framework.authtoken',
+    'rest_framework_simplejwt',
     'djoser',
 ]
 
@@ -101,7 +101,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.JWTAuthentication',
     ],
 
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
